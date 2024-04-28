9e25552d71e8b35799122f4ab29bc2ce38f1ca1f
syleung <syleung1220@gmail.com>
2024-04-28 18:37:03 +0800

    add upload media component

--- cloud_chatbot.py
+++ cloud_chatbot.py
@@ -12,6 +12,13 @@ from openai import AzureOpenAI
 
 # "st.session_state object:", st.session_state      # for testing
 
+apis = {
+    "CHAT": "https://httpbin.org/get",# "https://v1/chat",
+    "TRANSLATE": "https://httpbin.org/get",# "https://v1/translate",
+    "DRAW":  "https://httpbin.org/get",# "https://v1/media",
+    "MUSIC": "https://httpbin.org/get",#  "https://v1/music",
+}
+
 # ------ USER AUTHENTICATION ----- #
 # Import the YAML file
 file_path = Path(__file__).parent / "user_credentials.yaml"
@@ -45,11 +52,17 @@ if st.session_state["authentication_status"]: # USER AUTHENTICATION is success
         st.session_state['mode'] = 'CHAT'
 
     with st.sidebar:
+        # buttons
+        authenticator.logout()  
         chat_bu = st.button('💬 Chat', use_container_width=100)
         translate_bu = st.button('🗣️ Translate', use_container_width=100)
         draw_bu = st.button('🎨 Draw', use_container_width=100)
         music_bu = st.button('🎵 Music', use_container_width=100)
 
+        openai_api_key = st.text_input("Azure OpenAI API Key", key="chatbot_api_key", type="password")
+        "[Get an Azure OpenAI API key](https://itsc.hkust.edu.hk/services/it-infrastructure/azure-openai-api-service)"
+
+        # switch between different mode
         if chat_bu:
             st.session_state['mode'] = 'CHAT'
         if translate_bu:
@@ -59,15 +72,42 @@ if st.session_state["authentication_status"]: # USER AUTHENTICATION is success
         if music_bu:
             st.session_state['mode'] = 'MUSIC'
 
-        openai_api_key = st.text_input("Azure OpenAI API Key", key="chatbot_api_key", type="password")
-        "[Get an Azure OpenAI API key](https://itsc.hkust.edu.hk/services/it-infrastructure/azure-openai-api-service)"
-
     model_name = "gpt-35-turbo"
 
     if st.session_state['mode'] == 'CHAT':
         st.title("💬 Chatbot")
-
-
+        # --------- upload media -------------
+        uploaded_files = st.file_uploader(
+            label=":frame_with_picture: upload image", 
+            type=['png', 'jpg'],
+            accept_multiple_files=True
+        )
+        
+        for uploaded_file in uploaded_files:
+            bytes_data = uploaded_file.read()
+            st.write("filename:", uploaded_file.name)
+            st.write(bytes_data)
+        
+        # --------- sending requests ---------
+        d = {
+                'id': 'value1', 
+                'userId': 'value2',
+                'sessionId': 'CHAT',
+                'input':{
+                    'role': 'user',
+                    'content': {
+                        'type': 'text',
+                        'url': 'url',
+                        'prompt': 'prompt'
+                    }
+                }
+            }
+        test = {"firstName": "John", "lastName": "Smith"}
+        r = requests.post(apis.get(st.session_state['mode']), data=test)
+        st.caption("print r: "+r.text)
+        # --------- sending requests ---------
+        
+        # --------- from original code ---------
         if "messages" not in st.session_state:
             st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -98,7 +138,7 @@ if st.session_state["authentication_status"]: # USER AUTHENTICATION is success
             msg = response.choices[0].message.content
             st.session_state.messages.append({"role": "assistant", "content": msg})
             st.chat_message("assistant").write(msg)
-
+            # --------- from original code end ---------
     elif st.session_state['mode'] == 'TRANSLATE':
         st.title("🗣️ Translate")
     elif st.session_state['mode'] == 'DRAW':
